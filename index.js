13252f843cdd9337a5573fd0116b0ed110e9d3d6
Joonas <joonas_partanen@hotmail.com>
2021-10-06 20:46:42 +0300

    asd

--- index.js
+++ index.js
@@ -3,20 +3,31 @@ const bodyParser = require('body-parser')
 const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
 const bcrypt = require('bcryptjs')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
 
-let useDB = [];
+let userDB = [];
+let items = [];
 
 app.use(bodyParser.json());
 
 passport.use(new BasicStrategy(
     (username, password, done) => {
-        const searchResult = userDB.find(user => ((username === user.username) && (password === user.password)))
-        if(searchResult != undefined){
+        const searchResult = userDB.find(user => {
+            if (user.username === username){
+                if(bcrypt.compareSync(password, user.password)){
+                    return true;
+                }
+            }
+            return false;
+        })
+            console.log(searchResult);
+        if (searchResult != undefined) {
             done(null, searchResult);
         } else {
-            done(null, false);
+        done(null, false);
         }
     }));
 
@@ -28,8 +39,24 @@ app.get('/GetItems', (req, res) => {
 
 })
 
-app.post('/PostItem', (req, res) => {
+app.post('/PostItem', passport.authenticate('basic', {session:false}), (req, res) => {
     
+    /*const newItem = [
+        {
+            "ID": req.body.id,
+            "title": req.body.title,
+            "description": req.body.description,
+            "category": req.body.category,
+            "location": req.body.location,
+            "price": req.body.price,
+            "date": req.body.date,
+            "deliveryType": req.body.deliveryType,
+            "sellerName": req.body.sellerName,
+            "sellerEmail": req.body.sellerEmail,
+          }
+        ]
+        items.push(newItem);*/
+        res.sendStatus(201)
 })
 
 app.get('/GetItem/:location?/:category?/:date?', (req, res) => {
@@ -41,7 +68,17 @@ app.post('/DeleteItem', (req, res) => {
 })
 
 app.post('/SignUp', (req, res) => {
-    const
+    const salt = bcrypt.genSaltSync(6);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+    
+    const newUser = {
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+    }
+
+    userDB.push(newUser);
+    res.sendStatus(201);
 })
 
 app.post('/LogIn', (req, res) => {
